20b49bfd2a51f693fc3e90de3f36b92f76b28b9f
dmarmijosa <daniel2667@hotmail.es>
2020-09-28 19:46:46 -0500

    login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -9,14 +9,32 @@ process.env.PORT = process.env.PORT || 3000;
 //================
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+// ===============
+// Vencimiento del Token
+//================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 dias
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+// ===============
+// SEED de autentificacion
+//================
+
+
+// ===============
+// base de datos remota
+//================
 
 let urlDB;
 
-/* if (process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
-} else { */
-urlDB = 'mongodb+srv://dmarmijosa:7pwmHs522eE3LIYP@cluster0.cfcmt.mongodb.net/cafe?retryWrites=true&w=majority'
-    //}
+} else {
+    urlDB = 'mongodb+srv://dmarmijosa:7pwmHs522eE3LIYP@cluster0.cfcmt.mongodb.net/cafe?retryWrites=true&w=majority'
+}
 
 process.env.URLDB = urlDB;
 
--- server/server.js
+++ server/server.js
@@ -7,13 +7,14 @@ const app = express();
 
 
 const bodyParser = require('body-parser');
-
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
-app.use(require('./routes/usuario'));
 
+// configuración global de rutas
+app.use(require('./routes/index'));
 
 mongoose.connect(process.env.URLDB + '', { useFindAndModify: false, useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true }, (err, res) => {
     if (err) {
